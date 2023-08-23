7343befac039b2d8d05270ba30e3097cd404168a
Mahek <mmahekk24@gmail.com>
2023-08-23 21:03:01 +0530

    Linked List

---
+++ Linked List/intro.cpp
@@ -0,0 +1,10 @@
+// Linked lists:
+// chain of nodes:
+// data(user defined data) + pointer(addresss of  next node)
+//singly linked list
+// the node whose address is known is caleed head, and thhe further nodes have address of next nodes. the node which has null addrees, is called tail, i.e. last node.
+//doublee linked list:
+// its node has 3 pointers and 1 data, 1st pointer points towards the previous node and second pointer points towards next node.
+//the 2nd pointer of last node points towards  null
+//circular linked list
+// this is same as singly just the lastnode's pointer points towards the address
