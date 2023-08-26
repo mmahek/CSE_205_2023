99e5c526727724952246705e1be1f93579ac6b82
Mahek <mmahekk24@gmail.com>
2023-08-26 13:43:04 +0530

    hg

--- Linked List/singlyLL.cpp
+++ Linked List/singlyLL.cpp
@@ -1,73 +1,76 @@
-    #include <iostream>
-    using namespace std;
+#include <iostream>
+using namespace std;
 
-    class Node
-    {
-    public:
-        int data;
-        Node *next; // this pointer will point to next node
+class Node
+{
+public:
+    int data;
+    Node *next; // this pointer will point to next node
 
-        Node(int val)
-        { // constructor method, named same as class. no return type0
-            data = val;
-            next = NULL;
-        }
-    };
-
-    void check(Node* &head, int what){
-        Node* temp = head;
-        // cout << "what :" ;
-        // cin >> what;
-        while(temp!= NULL){
-            if (temp->data == what ){
-                cout << "True" << endl;
-            }
-            temp = temp->next;
-        }
+    Node(int val)
+    { // constructor method, named same as class. no return type0
+        data = val;
+        next = NULL;
     }
+};
 
-    void insertAtTail(Node *&head, int val)
-    { // created a pointer variable and passed the value by reference because we want to make changes to the original linked list
-        Node *n = new Node(val);
-        if (head == NULL)
+void check(Node *&head, int what)
+{
+    Node *temp = head; // pointer in memory for changing its value and traversing through the linked list
+    // cout << "what :" ;
+    // cin >> what;
+    while (temp != NULL)
+    {
+        if (temp->data == what) // data of node
         {
-            head = n;
-            return;
+            cout << "True" << endl;
         }
-        Node *temp = head;
-        while (temp->next != NULL)
-        { //-> arrow operator
-            temp = temp->next;
-        }
-        temp->next = n;
+        temp = temp->next; // otherwise, go to next node
     }
+}
 
-    void insertAtHead(Node *&head, int val)
+void insertAtTail(Node *&head, int val)
+{ // created a pointer variable and passed the value by reference because we want to make changes to the original linked list
+    Node *n = new Node(val);
+    if (head == NULL)
     {
-        Node *n = new Node(val);
-        n->next = head;
         head = n;
+        return;
     }
+    Node *temp = head;
+    while (temp->next != NULL)
+    { //-> arrow operator
+        temp = temp->next;
+    }
+    temp->next = n; // n is new node
+}
 
-    void display(Node *head)
-    {
-        Node *temp = head;
+void insertAtHead(Node *&head, int val)
+{
+    Node *n = new Node(val);
+    n->next = head; // connection is made by pointing the
+    head = n;
+}
 
-        while (temp != NULL)
-        {
-            cout << temp->data << "->";
-            temp = temp->next;
-        }
-    }
-    int main()
+void display(Node *head)
+{
+    Node *temp = head;
+
+    while (temp != NULL)
     {
-        Node *head = NULL; // empty linked list created
-        // head never moves or changes
-        // instead create a new pointer temp pointing to head and change it till it reaches the last pointer. Keep iterating temp to reach the last
-        //  insertAtTail(head, 2);//new node has been created in memory
-        insertAtTail(head, 1);
-        insertAtTail(head, 2);
-        insertAtHead(head, 10);
-        display(head);
-        check(head, 2);
+        cout << temp->data << "->";
+        temp = temp->next;
     }
+}
+int main()
+{
+    Node *head = NULL; // empty linked list created
+    // head never moves or changes
+    // instead create a new pointer temp pointing to head and change it till it reaches the last pointer. Keep iterating temp to reach the last
+    //  insertAtTail(head, 2);//new node has been created in memory
+    insertAtTail(head, 1);
+    insertAtTail(head, 2);
+    insertAtHead(head, 10);
+    display(head);
+    check(head, 2);
+}
