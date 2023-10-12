d50bf7c016fb9726c1b49c3202fa3011da2df4ae
Mahek <114944166+mmahek@users.noreply.github.com>
2023-10-12 19:35:23 +0530

    Traversal

--- Trees/binaryTrees.cpp
+++ Trees/binaryTrees.cpp
@@ -1,15 +1,17 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Node{
-    public:
-    //data field, left pointer, right pointer
+class Node
+{
+public:
+    // data field, left pointer, right pointer
     int data;
-    Node* left;
-    Node* right;
+    Node *left;
+    Node *right;
 
-    //constructor
-    Node(int val){
+    // constructor
+    Node(int val)
+    {
         data = val;
         left = NULL;
         right = NULL;
@@ -25,36 +27,40 @@ void postOrder(Node *root)
     postOrder(root->right);
 }
 
-void inOrder(Node* root){// left  root  right
+void inOrder(Node *root)
+{ // left  root  right
     if (root == NULL)
         return;
 
     inOrder(root->left);
-    cout << root->data << " ";   
+    cout << root->data << " ";
     inOrder(root->right);
 }
 
-//Preorder Traversal
-void preOrder(Node* root){// Root  left  right
-    //base case: reached the end of branch
-    if(root == NULL) return;
+// Preorder Traversal
+void preOrder(Node *root)
+{ // Root  left  right
+    // base case: reached the end of branch
+    if (root == NULL)
+        return;
 
     cout << root->data << " ";
     preOrder(root->left);
     preOrder(root->right);
 }
 
-int main(){
-    Node* root = new Node(1);//root node created, first circle, currently, left and right pointer of this node points to NULL
-    root -> left = new Node(2);//new node has been created to the left of root node 
-    root -> right = new Node(3);
-    root -> left -> left = new Node(4);//right to the left of root
-    root -> left -> right = new Node(5);
+int main()
+{
+    Node *root = new Node(1); // root node created, first circle, currently, left and right pointer of this node points to NULL
+    root->left = new Node(2); // new node has been created to the left of root node
+    root->right = new Node(3);
+    root->left->left = new Node(4); // right to the left of root
+    root->left->right = new Node(5);
 
-    root -> right -> left = new Node(6);//child of node 3 at left
-    root -> right -> right = new Node(7);// child of node 3 at right
+    root->right->left = new Node(6);  // child of node 3 at left
+    root->right->right = new Node(7); // child of node 3 at right
 
-    //traversal of tree
+    // traversal of tree
     cout << "The traversal of tree by PreOrder: ";
     preOrder(root);
 
---
+++ Trees/levelOrderTraversal.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+class Node{
+    public: 
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void printLevelOrder(Node* root){
+
+    if(root == NULL) return;
+    queue<Node*> q;
+    q.push(root);
+    q.push(NULL);
+
+    while(!q.empty()){
+        Node* node = q.front();
+        q.pop();
+
+        if(node != NULL){
+            cout << node->data << " ";
+            if(node->left){
+                q.push(node->left);
+            }
+            if(node->right){
+                q.push(node->right);
+            }
+        }
+        else if(!q.empty()){
+            q.push(NULL);
+        }
+    }
+}
+
+
+int main(){
+    Node *root = new Node(1); // root node created, first circle, currently, left and right pointer of this node points to NULL
+    root->left = new Node(2); // new node has been created to the left of root node
+    root->right = new Node(3);
+    root->left->left = new Node(4); // right to the left of root
+    root->left->right = new Node(5);
+
+    root->right->left = new Node(6);  // child of node 3 at left
+    root->right->right = new Node(7); // child of node 3 at right
+
+
+    printLevelOrder(root);
+    return 0;
+}
